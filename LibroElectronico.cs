4d59e9fe45a0a5ae784e9ff1985a1ff80e39b207
Douglas Morales <DouGMoralesVelasquez@hotmail.com>
2024-05-23 08:39:49 -0600

    Agregar archivos de proyecto.

---
+++ LibroElectronico.cs
@@ -0,0 +1,14 @@
+﻿namespace TercerParcial
+{
+    public class LibroElectronico : Libro
+    {
+        public string Formato {  get; set; }
+        public string Tamanio { get; set; }
+
+        public LibroElectronico() 
+        { 
+            Formato = string.Empty;
+            Tamanio = string.Empty;
+        }
+    }
+}
---
+++ Revista.cs
@@ -0,0 +1,14 @@
+﻿namespace TercerParcial
+{
+    public class Revista : Libro
+    {
+        public string Volumen {  get; set; }
+        public string NumEjemplar { get; set; }
+
+        public Revista()
+        {
+            Volumen = string.Empty;
+            NumEjemplar = string.Empty;
+        }
+    }
+}
